a36d0cd95216476e93e9c21656f1bc8faf91ae79
avantis <uaruscom@gmail.com>
2017-12-11 13:17:57 +0200

    Update ReadOneLine.php

--- tests/ReadOneLine.php
+++ tests/ReadOneLine.php
@@ -1,21 +1,21 @@
 <?php 
 /**
-*	This file is part of the joomimart.csv library
+*	This file is part of the pllano.csv library
 *
 *	@license http://opensource.org/licenses/MIT
-*	@link https://github.com/joomimart/csv
-*	@version 1.0.1
-*	@package joomimart.csv
+*	@link https://github.com/pllano/csv
+*	@version 1.0.2
+*	@package pllano.csv
 *
 *	For the full copyright and license information, please view the LICENSE
 *	file that was distributed with this source code.
 */
 	//	Include Composer autoloader if not already done.
 	require 'vendor/autoload.php';
-	//	require_once '/vendor/joomimart/csv/src/Reader.php';
+	//	require_once '/vendor/pllano/csv/src/Reader.php';
 	//	require_once __DIR__.'/src/Reader.php';
 	$filename = 'test.csv';
-	$csv = new joomiMart\Csv\Reader($filename);
+	$csv = new Pllano\Csv\Reader($filename);
 	$csv->setItemStart(10);
 	$csv->setExecute(1);
 	$records = $csv->Read();
