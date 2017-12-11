c2f1bcc74231d7ac574d649213321abcd8e18f4b
avantis <uaruscom@gmail.com>
2017-12-11 13:17:29 +0200

    Update QuickStart.php

--- tests/QuickStart.php
+++ tests/QuickStart.php
@@ -1,20 +1,20 @@
 <?php 
 /**
-*	This file is part of the joomimart.csv library
+*	This file is part of the pllano.csv library
 *
 *	@license http://opensource.org/licenses/MIT
-*	@link https://github.com/joomimart/csv
+*	@link https://github.com/pllano/csv
 *	@version 1.0.1
-*	@package joomimart.csv
+*	@package pllano.csv
 *
 *	For the full copyright and license information, please view the LICENSE
 *	file that was distributed with this source code.
 */
 	require 'vendor/autoload.php';
-	//	require_once '/vendor/joomimart/csv/src/Reader.php';
+	//	require_once '/vendor/pllano/csv/src/Reader.php';
 	//	require_once __DIR__.'/src/Reader.php';
 	$filename = 'test.csv';
-	$csv = new joomiMart\Csv\Reader($filename);
+	$csv = new Pllano\Csv\Reader($filename);
 	$records = $csv->Read();
 	$count = count($records);
 	if ($count >= 1) {
